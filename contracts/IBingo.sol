db63fb5cd03f7ed57fe09fa960c026835e7602ae
Christiaan Koopal <104635072+0xchristiaan@users.noreply.github.com>
2022-06-13 15:45:05 +0200

    Delete IBingo.sol

--- contracts/IBingo.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
-
-interface IBingo {
-    function joinGame() external;
-    function startGame() external;
-    function drawNumber() external;
-    function claimBingo() external;
-}
