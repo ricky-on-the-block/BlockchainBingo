c67d3a7d3820e61555545f9aacbc9d59ef9d2d1e
christiaan koopal <0xchristiaan@gmail.com>
2022-06-29 15:17:35 +0200

    hardhatconfig

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,27 +1,28 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config({ path: ".env" });
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+const RINKEBY_ALCHEMY_API_KEY_URL = process.env.RINKEBY_ALCHEMY_API_KEY_URL;
+const ROPSTEN_ALCHEMY_API_KEY_URL = process.env.ROPSTEN_ALCHEMY_API_KEY_URL;
+const GOERLI_ALCHEMY_API_KEY_URL = process.env.ROPSTEN_ALCHEMY_API_KEY_URL;
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: "0.8.9",
   networks: {
-    // localhost: {
-    //   url: "http://127.0.0.1:8545"
-    // }
+    ropsten: {
+      url: ROPSTEN_ALCHEMY_API_KEY_URL,
+      accounts: [PRIVATE_KEY],
+    },
+    rinkeby: {
+      url: RINKEBY_ALCHEMY_API_KEY_URL,
+      accounts: [PRIVATE_KEY],
+    },
+    goerli: {
+      url: RINKEBY_ALCHEMY_API_KEY_URL,
+      accounts: [PRIVATE_KEY]
+    },
+
   }
 };
